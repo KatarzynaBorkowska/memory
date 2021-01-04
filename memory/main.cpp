71180fb374fc92f53e9c36969328f79599882178
KatarzynaBorkowska <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-01-04 14:17:42 +0100

    Update main.cpp

--- memory/main.cpp
+++ memory/main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 int main() {
-    int n,w,p,i;
+    int n,w,p,d,i;
     vector<int> v1,v2;
     srand( time( NULL ) );
     cout << "Witaj w grze memory!!!\n";
@@ -24,31 +24,64 @@ int main() {
     while(i != 0)
     {
         p = ( rand() % w ) + 1;
-        cout << "p -> " << p << endl;
         if(czy_juz_byla(v1, p)){
             v1.push_back(p);
             i--;
             continue;
         }
-        cout << "Liczba sie powtorzyła " << p << endl;
     }
     i = w;
     while(i != 0)
     {
         p = ( rand() % w ) + 1;
-        cout << "p -> " << p << endl;
         if(czy_juz_byla(v2, p)){
             v2.push_back(p);
             i--;
             continue;
         }
-        cout << "Liczba sie powtorzyła " << p << endl;
     }
     for(i = 0;i < w;i++)
         cout << v1[i] << " ";
     cout << endl;
     for(i = 0;i < w;i++)
         cout << v2[i] << " ";
+    n = w;
+    while(n != 0)
+    {
+        cout << endl << "Podaj dwa pola ktore chcesz odsłonić: (w przedziale od 0 do " << w << ") " << endl;
+        
+        cout << "v1 -> ";
+        for(i = 0;i < w;i++){
+            cout << i + 1 << ". ";
+            if(v1[i] == 0)
+                cout << v1[i] << " ";
+            else
+                cout << "  ";
+        }
+        cout << endl << "v2 -> ";
+        for(i = 0;i < w;i++){
+            cout << i + 1 << ". ";
+            if(v2[i] == 0)
+                cout << v2[i] << " ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+        cin >> p;
+        cin >> d;
+        p--;
+        d--;
+        if(v1[p] == v2[d])
+        {
+            v1[p] = 0;
+            v2[d] = 0;
+            n--;
+        }
+        else
+            cout << "Niestety nie trafiles\n";
+        system("clear");
+    }
+    cout << "Brawo znalazles wszystkie pary!!!!!\n";
     
     return 0;
 }
