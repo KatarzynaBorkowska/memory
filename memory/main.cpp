bd26cf383ef02862de1866c6e80c73e1ac6997d7
Kasia <kasiaboekowska17@gmail.com>
2021-01-04 13:37:49 +0100

    Initial Commit

---
+++ memory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  memory
+//
+//  Created by Kasia on 04/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
